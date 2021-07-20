d95053d52ba5f55adebb52a4ab09234251d97957
wolver <gn_wolver@hotmail.com>
2021-07-20 14:12:54 -0500

    Subi el archivo de la practica

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDRFoOEt36BQTfAdX69Z9RB1LbXENgNpXI",
+  authDomain: "chatdwp.firebaseapp.com",
+  projectId: "chatdwp",
+  storageBucket: "chatdwp.appspot.com",
+  messagingSenderId: "596054803702",
+  appId: "1:596054803702:web:2eb541bf84510a206bd8d7",
+  measurementId: "G-YTKR711Y8Z"
+    });
