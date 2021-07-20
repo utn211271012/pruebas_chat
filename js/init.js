81e6ace661eea6337c1f8dbe9ebcb1d7a1448d19
wolver <gn_wolver@hotmail.com>
2021-07-20 14:54:04 -0500

    modificaciones en las credenciales digitales

--- js/init.js
+++ js/init.js
@@ -1,12 +1,8 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDRFoOEt36BQTfAdX69Z9RB1LbXENgNpXI",
-  authDomain: "chatdwp.firebaseapp.com",
-  projectId: "chatdwp",
-  storageBucket: "chatdwp.appspot.com",
-  messagingSenderId: "596054803702",
-  appId: "1:596054803702:web:2eb541bf84510a206bd8d7",
-  measurementId: "G-YTKR711Y8Z"
+    apiKey: "AIzaSyD1_snA7OglLi0Z7WX5ClfsiPEv9qorEFM",
+    authDomain: "chat801sror.firebaseapp.com",
+    projectId: "chat801sror",
+    storageBucket: "chat801sror.appspot.com",
+    messagingSenderId: "968713191111",
+    appId: "1:968713191111:web:4010d66900c70d495bc8e2"
     });
